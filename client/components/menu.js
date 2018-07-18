edecb28ac0332689c3199e0f7bcfd8079d6adbf8
Elyse Vest <evest90@gmail.com>
2018-07-18 15:12:12 -0500

    Make login, signup, and rules all toggle on a button click

--- client/components/menu.js
+++ client/components/menu.js
@@ -1,10 +1,37 @@
 angular.module('app')
-.controller('menuCtrl', (/*requiered services live here*/) => {
+    .controller('menuCtrl', function () {
+        console.log('hello from menu js');
+        this.showLoginForm = false;
+        this.showSignUpForm = false;
+        this.showRules = false;
 
-})
-.component('menu', {
-  bindings : {},
-  controller : 'menuCtrl',
-  templateUrl : '/templates/menu.html'
+        this.toggleLoginForm = () => {
+            if (this.showLoginForm) {
+                this.showLoginForm = false;
+            } else {
+                this.showLoginForm = true;
+            }
+        };
 
-})
+        this.toggleSignUpForm = () => {
+            if (this.showSignUpForm) {
+                this.showSignUpForm = false;
+            } else {
+                this.showSignUpForm = true;
+            }
+        };
+
+        this.toggleRules = () => {
+            if (this.showRules) {
+                this.showRules = false;
+            } else {
+                this.showRules = true;
+            }
+        }
+    })
+    .component('menu', {
+        bindings : {},
+        controller : 'menuCtrl',
+        templateUrl : '/templates/menu.html'
+
+    });
