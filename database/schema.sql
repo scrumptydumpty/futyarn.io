701501615681366e9902c801f4a878f60189e670
Clayton Lin <clayton.yt.lin@gmail.com>
2018-07-18 16:04:38 -0500

    Update db schema and sample data

--- database/schema.sql
+++ database/schema.sql
@@ -1,36 +1,40 @@
 -- CREATE SCHEMA fyschme
 -- Explicit schema not necessary since we only have single user (app server)
   
-  DROP TABLE IF EXISTS players
+DROP TABLE IF EXISTS players
 
-  CREATE TABLE players (
-      user_id      INTEGER AUTO_INCREMENT PRIMARY KEY,
-      username     VARCHAR(255) NOT NULL UNIQUE,
-      password     VARCHAR(255) NOT NULL,
-      wins         INTEGER NOT NULL,
-      losses       INTEGER NOT NULL,
-      games_played INTEGER NOT NULL,
-      goals        INTEGER NOT NULL, 
-      CHECK ((wins + losses) = games_played)
-  )
-  
-  DROP TABLE IF EXISTS games
+-- Postgres uses SERIAL instead of AUTO_INCREMENT. If using SERIAL, do not have to use INTERGER.
+-- PG does not need FOREIGN KEY property. Using REFERENCES is enough.
+-- CHECK establishes a constraint. If condition not met, throws error.
 
-  CREATE TABLE games ( 
-      game_id            INTEGER AUTO_INCREMENT PRIMARY KEY,
-      team_one_one       INTEGER REFERENCES players(user_id),
-      team_one_two       INTEGER REFERENCES players(user_id),
-      team_one_three     INTEGER REFERENCES players(user_id),
-      team_one_four      INTEGER REFERENCES players(user_id),
-      team_one_five      INTEGER REFERENCES players(user_id),
-      team_two_one       INTEGER REFERENCES players(user_id),
-      team_two_two       INTEGER REFERENCES players(user_id),
-      team_two_three     INTEGER REFERENCES players(user_id),
-      team_two_four      INTEGER REFERENCES players(user_id),
-      team_two_five      INTEGER REFERENCES players(user_id),
-      team_one_score     INTEGER NOT NULL,
-      team_two_score     INTEGER NOT NULL,
-      winning_team       VARCHAR(10) NOT NULL,
-      losing team        VARCHAR(10) NOT NULL,
-      cat_of_the_game    INTEGER REFERENCES players(user_id)
-  )
+CREATE TABLE players (
+    user_id      SERIAL PRIMARY KEY,
+    username     VARCHAR(255) NOT NULL UNIQUE,
+    password     VARCHAR(255) NOT NULL,
+    wins         INTEGER NOT NULL,
+    losses       INTEGER NOT NULL,
+    games_played INTEGER NOT NULL,
+    goals_made   INTEGER NOT NULL, 
+    CHECK ((wins + losses) = games_played)
+)
+
+DROP TABLE IF EXISTS games
+
+CREATE TABLE games ( 
+    game_id            SERIAL PRIMARY KEY,
+    team_one_one       INTEGER REFERENCES players(user_id) NULL,
+    team_one_two       INTEGER REFERENCES players(user_id) NULL,
+    team_one_three     INTEGER REFERENCES players(user_id) NULL,
+    team_one_four      INTEGER REFERENCES players(user_id) NULL,
+    team_one_five      INTEGER REFERENCES players(user_id) NULL,
+    team_two_one       INTEGER REFERENCES players(user_id) NULL,
+    team_two_two       INTEGER REFERENCES players(user_id) NULL,
+    team_two_three     INTEGER REFERENCES players(user_id) NULL,
+    team_two_four      INTEGER REFERENCES players(user_id) NULL,
+    team_two_five      INTEGER REFERENCES players(user_id) NULL,
+    team_one_score     INTEGER NOT NULL,
+    team_two_score     INTEGER NOT NULL,
+    winning_team       VARCHAR(10) NOT NULL,
+    losing_team        VARCHAR(10) NOT NULL,
+    cat_of_the_game    INTEGER REFERENCES players(user_id)
+)
