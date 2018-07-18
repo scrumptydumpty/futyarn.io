e032382679fa49d785f8b78f8d4b513d6a5a5ae3
Hack Reactor Student <student@hackreactor.com>
2018-07-18 11:47:26 -0500

    Initialize Canvas

---
+++ gameClient/components/gamecanvas.js
@@ -0,0 +1,36 @@
+angular.module('gameinstance')
+.controller('gamecanvasCtrl', (/*requiered services live here*/) => {
+
+})
+.directive('anim', () =>
+    {   
+         return {
+            //Restrict Directive invocation to Attribute on Element
+            restrict: "A",
+            //Link registers Dom listeners and can update DOM
+            //executed after template is cloned
+            link : (scope, element, attrs, controller, transcludeFn) =>
+                {
+                    // scope is an AngularJS scope object.
+                    // element is the jqLite-wrapped element that this directive matches.
+                    // attrs is a hash object with key-value pairs of normalized attribute names and their corresponding attribute values.
+                    // controller is the directive's required controller instance(s) or its own controller (if any). The exact value depends on the directive's require property.
+                    // transcludeFn is a transclude linking function pre-bound to the correct transclusion scope.
+                    
+                    //el[0] === the canvas object we drop the directive on
+                    var canvas = element[0]
+                    //fetch 2d context for canvas to draw
+                    var ctx = canvas.getContext('2d')
+                    
+                    ctx.fillStyle = "LightGreen"
+                    ctx.fillRect(0,0,canvas.width,canvas.height);
+                }
+            
+        }
+    })
+.component('gamecanvas', {
+    bindings : {},
+    controller : 'gamecanvasCtrl',
+    templateUrl : '/templates/gamecanvas.html'
+
+})
--- servers/gameServer.js
+++ servers/gameServer.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
+var port = 1337
 
 var gameInstance = express()
 
-app.use(express.static(__dirname + '/../node_modules'))
-app.use(express.static(__dirname + '/../client'))
+gameInstance.use(express.static(__dirname + '/../gameClient'))
+gameInstance.use(express.static(__dirname + '/../node_modules'))
 
 
-gameInstance.listen(1337, () => {
-
-})
+gameInstance.listen(port, () => 
+    {
+        console.log(`Game Instance Server running on port ${port}`)
+    })
