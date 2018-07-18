8a75b14809060e7143fa106d8e028dad23867d3c
Clayton Lin <clayton.yt.lin@gmail.com>
2018-07-18 14:34:58 -0500

    Add comments to list needed function

--- database/postgreSQL-index.js
+++ database/postgreSQL-index.js
@@ -9,4 +9,44 @@ var pg = require('knex')({
     }
 });
 
+// PGHOST
+// PGPORT
+
+// FUNCTIONS NEEDED
+
+// addNewUser
+
+// getUserInfo
+
+// updateUserInfo
+
+// getLeaderboard
+
+// addGameInfo
+
+// getGameInfo
+
+
+
+
+
+
+// OPERATIONS EXAMPLES
+
+// insert single entry
+// INSERT INTO players () VALUES ();
+
+// insert multiple entries
+// INSERT INTO products (product_no, name, price) VALUES
+//     (1, 'Cheese', 9.99),
+//     (2, 'Bread', 1.99),
+//     (3, 'Milk', 2.99);
+
+// update examples
+// UPDATE products SET price = 10 WHERE price = 5;
+// UPDATE products SET price = price * 1.10;
+// UPDATE mytable SET a = 5, b = 3, c = 1 WHERE a > 0;
+
+
+
 //define queries in here on the pg thing (export said methods)
