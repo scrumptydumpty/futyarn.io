3928ef41ec1f1bdcea4d896aadb70900cd0a04b5
Clayton Lin <clayton.yt.lin@gmail.com>
2018-07-18 10:52:03 -0500

    Add changes to please linter

--- servers/routes.js
+++ servers/routes.js
@@ -1,31 +1,34 @@
-var express = require('express')
+var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => {
-    //should serve static html
-  
-});
 
+
+// /api/login
 router.get('/login', (req, res, next) => {
     //hit up that DB
     
-    res.setStatus = 200
-  res.end('<div><div> hello!!!  </div> </div>')
+    res.setStatus = 200;
+    res.end('<div><div> hello!!!  </div> </div>');
 });
 
 router.get('/leaderboards', (req, res, next) => {
     //hit up that DB
-  
+    
 });
 
 router.post('/signup', (req, res, next) => {
-   //hit up that DB
-  
+    //hit up that DB
+    
 });
 
 router.get('/joingame', (req, res, next) => {
     //hit up russell's server
-  
+    
 });
 
-module.exports.router = router
+// router.get('/*', (req, res, next) => {
+//     //should serve static html
+  
+// });
+
+module.exports.router = router;
--- servers/routing-server.js
+++ servers/routing-server.js
@@ -1,16 +1,16 @@
 var express = require('express');
-var routes = require('./routes.js')
+var routes = require('./routes.js');
 
 
-var app = express()
+var app = express();
 
 
-app.use(express.static(__dirname + '/../client'))
-app.use(express.static(__dirname + '/../node_modules'))
+app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../node_modules'));
 
-app.use('/', routes.router) // maybe '/*' insead of '/'
+app.use('/', routes.router); // maybe '/*' insead of '/'
 
 
 app.listen(3000, () => {
-  console.log('routing server listening on port 3k')
-})
+    console.log('routing server listening on port 3k');
+});
