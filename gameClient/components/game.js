0647b1ebb43c24508139d1a16597134355883be0
Hack Reactor Student <student@hackreactor.com>
2018-07-18 11:47:26 -0500

    Initialize Canvas

---
+++ gameClient/components/game.js
@@ -0,0 +1,9 @@
+angular.module('gameinstance')
+.controller('gameCtrl', (/*required services go in here*/)=>{
+
+})
+.component('game', {
+  bindings : {},
+  controller : 'gameCtrl',
+  templateUrl : './templates/game.html'
+})
