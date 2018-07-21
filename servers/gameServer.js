83e10a3698a26f90212588428890565fd07cce32
Russell Grindstaff <RussellGrindstaff@gmail.com>
2018-07-21 15:14:09 -0500

    add sockets

--- servers/gameServer.js
+++ servers/gameServer.js
@@ -1,14 +1,39 @@
 var express = require('express');
+var gameInstance = express()
+var path = require('path')
+var http = require('http').createServer(gameInstance)
+var io = require('socket.io')(http)
 
 var port = 1337
 
-var gameInstance = express()
+
+var cache = {
+	ballLoc : {
+		cx: 300,
+		cy: 300,
+		dx: 0,
+		dy: 0
+	}
+}
+
+var nextUserID = 0
 
 gameInstance.use(express.static(__dirname + '/../gameClient'))
 gameInstance.use(express.static(__dirname + '/../node_modules'))
 
+gameInstance.get('/', function(req, res){
 
-gameInstance.listen(port, () => 
+})
+
+io.on('connection', function(socket){
+	console.log('a user connected')
+	io.emit('hi', nextUserID)
+	nextUserID ++;
+
+})
+
+
+http.listen(port, () => 
     {
         console.log(`Game Instance Server running on port ${port}`)
     })
