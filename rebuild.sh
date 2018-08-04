e9a90df2cad2a705648d4810e8019780bd1db82e
Scott <scottgrimes89@gmail.com>
2018-08-04 09:31:31 -0500

    Fix sh file

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@ if [ "$1" == "client" ]; then
     webpack --config ./webpack.config.js && docker stop futyarn && docker rm futyarn && docker build -t futyarn . && docker run --name futyarn --network br0 -d futyarn 
 elif [ "$1" == "nocontainer" ]; then
     echo "Starting new container.."
-    docker build -t futyarn . && docker run --name futyarn --network br0 -d futyarn
+    webpack --config ./webpack.config.js && docker build -t futyarn . && docker run --name futyarn --network br0 -d futyarn
 else
 	echo "Restarting container..!"
     docker stop futyarn && docker rm futyarn && docker build -t futyarn . && docker run --name futyarn --network br0 -d futyarn
